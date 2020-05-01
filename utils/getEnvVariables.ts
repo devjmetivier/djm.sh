17d16da8de7f3d0f263b942c7183df3b6580a1a1
Devin Metivier <devin.metivier@lcecorp.com>
2020-05-01 09:02:48 -0400

    Buffer for private key

--- utils/getEnvVariables.ts
+++ utils/getEnvVariables.ts
@@ -2,7 +2,7 @@ export const getEnvVariables = () => {
   const type = process.env.type;
   const project_id = process.env.project_id;
   const private_key_id = process.env.private_key_id;
-  const private_key = process.env.private_key;
+  const private_key = new Buffer(process.env.private_key, "base64").toString();
   const client_email = process.env.client_email;
   const client_id = process.env.client_id;
   const auth_uri = process.env.auth_uri;
